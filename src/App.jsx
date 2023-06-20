827276ebda36ba0f8e02a31b5658af27dae9569e
Olga <lvnvrvn@yandex.ru>
2023-06-20 13:19:03 +0300

    добавлена библиотека

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {/* <header className="header">
+          <div className="header__logo"></div>
+          <div className="header__saved"></div>
+        </header> */}
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Logo
+            </Typography>
+            <Button color="inherit">Saved</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {/* <form className='search__form' action="/">
+        <input className='search__input' type="text" placeholder='укажите ингридиенты' />
+        <button className='search-btn'>Показать</button>
+        <br />
+        <input type="checkbox" />
+      </form> */}
+      <Box className='search__form'
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField className='search__input' id="outlined-basic" label="Outlined" variant="outlined" />
+        <Button className='search-btn' variant="contained">Contained</Button>
+      </Box>
     </>
   )
 }
